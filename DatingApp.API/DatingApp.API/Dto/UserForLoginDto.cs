c70473a0ada926859b4a28ebae54977ce4f2c440
Pascal Fossouo <kenfosso@gmail.com>
2020-08-11 15:08:21 -0300

    End of Section 3

---
+++ DatingApp.API/DatingApp.API/Dto/UserForLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dto
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DatingApp.API/DatingApp.API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public byte[] passwordHash { get; set; }
+
+        public byte[] passwordSalt { get; set; }
+    }
+}
